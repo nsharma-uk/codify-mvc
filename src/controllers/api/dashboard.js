687a6131ded95e4087b0d3aaf6cedfdac7e3f5d4
surajverma2587 <surajverma2587@gmail.com>
2022-07-04 20:28:13 +0100

    complete api controllers

--- src/controllers/api/dashboard.js
+++ src/controllers/api/dashboard.js
@@ -1,5 +1,19 @@
-const getDashboard = (req, res) => {
-  return res.json({ success: true, data: {} });
+const { Playlist } = require("../../models");
+
+const getDashboard = async (req, res) => {
+  try {
+    // FOR NOW: hard code user id
+    // Later, get user id from logged in session object
+    const userId = 1;
+
+    const data = await Playlist.findAll({ where: { id: userId } });
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all playlists | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
 module.exports = {
--- src/controllers/api/playlists.js
+++ src/controllers/api/playlists.js
@@ -1,13 +1,54 @@
-const getAllPlaylists = (req, res) => {
-  return res.json({ success: true, data: [] });
+const { Playlist } = require("../../models");
+
+const getAllPlaylists = async (req, res) => {
+  try {
+    const data = await Playlist.findAll({});
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all playlists | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
-const getSinglePlaylist = (req, res) => {
-  return res.json({ success: true, data: {} });
+const getSinglePlaylist = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await Playlist.findByPk(id);
+
+    if (!data) {
+      return res.status(404).json({ success: false });
+    }
+
+    return res.json({ success: true, data });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get all playlists | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
-const createPlaylist = (req, res) => {
-  return res.json({ success: true });
+const createPlaylist = async (req, res) => {
+  try {
+    const { title, imageUrl } = req.body;
+
+    // FOR NOW: hard code user id
+    // Later, get user id from logged in session object
+    const userId = 1;
+
+    await Playlist.create({
+      title,
+      imageUrl,
+      userId,
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create playlist | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
 };
 
 module.exports = {
