931e66cffc4d9cf9d21c2e5e6e74ef8bab7ddf64
surajverma2587 <surajverma2587@gmail.com>
2022-07-04 22:03:43 +0100

    login and signup

---
+++ public/assets/js/index.js
@@ -0,0 +1,103 @@
+const signupForm = $("#signup-form");
+const loginForm = $("#login-form");
+
+const renderError = (id, message) => {
+  const errorDiv = $(`#${id}`);
+  errorDiv.empty();
+  errorDiv.append(`<div class="mb-3 text-center text-danger error-text">
+    ${message}
+  </div>`);
+};
+
+const handleSignup = async (event) => {
+  event.preventDefault();
+
+  const email = $("#email").val();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
+  const firstName = $("#firstName").val();
+  const lastName = $("#lastName").val();
+  const profileImageUrl = $("#profileImageUrl").val();
+
+  if (
+    email &&
+    password &&
+    confirmPassword &&
+    firstName &&
+    lastName &&
+    profileImageUrl
+  ) {
+    if (password === confirmPassword) {
+      try {
+        const payload = {
+          email,
+          password,
+          firstName,
+          lastName,
+          profileImageUrl,
+        };
+
+        const response = await fetch("/auth/signup", {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+          window.location.assign("/login");
+        } else {
+          renderError("signup-error", "Failed to create account. Try again.");
+        }
+      } catch (error) {
+        renderError("signup-error", "Failed to create account. Try again.");
+      }
+    } else {
+      renderError("signup-error", "Passwords do not match. Try again.");
+    }
+  } else {
+    renderError("signup-error", "Please complete all required fields.");
+  }
+};
+
+const handleLogin = async (event) => {
+  event.preventDefault();
+
+  const email = $("#email").val();
+  const password = $("#password").val();
+
+  if (email && password) {
+    try {
+      const payload = {
+        email,
+        password,
+      };
+
+      const response = await fetch("/auth/login", {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        window.location.assign("/dashboard");
+      } else {
+        renderError("login-error", "Failed to login. Try again.");
+      }
+    } catch (error) {
+      renderError("login-error", "Failed to login. Try again.");
+    }
+  } else {
+    renderError("login-error", "Please complete all required fields.");
+  }
+};
+
+signupForm.submit(handleSignup);
+loginForm.submit(handleLogin);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 const connection = require("./config/connection");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
 
+const hbs = exphbs.create({});
+
 const app = express();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
