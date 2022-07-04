16c09405129b9f69f8f656ab84e59d48b2417f3b
surajverma2587 <surajverma2587@gmail.com>
2022-07-04 20:13:49 +0100

    auth controller

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,6 +1,71 @@
-const login = (req, res) => {};
+const { User } = require("../../models");
 
-const signup = (req, res) => {};
+const login = async (req, res) => {
+  try {
+    // get the user data from payload
+    const { email, password } = req.body;
+
+    // get user by email address
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      console.log(
+        `[ERROR]: Failed to login | No user found with email: ${email}`
+      );
+
+      return res.status(500).json({ success: false });
+    }
+
+    const isAuthorised = await user.checkPassword(password);
+
+    if (isAuthorised) {
+      return res.json({ success: true });
+    } else {
+      console.log(
+        `[ERROR]: Failed to login | Incorrect password for email: ${email}`
+      );
+
+      return res.status(500).json({ success: false });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to login | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
+
+const signup = async (req, res) => {
+  try {
+    // get the user data from payload
+    const { firstName, lastName, email, password, profileImageUrl } = req.body;
+
+    // check if user exists
+    const user = await User.findOne({ where: { email } });
+
+    if (user) {
+      console.log(
+        `[ERROR]: Failed to create user | Account already exists with email: ${email}`
+      );
+
+      return res.status(500).json({ success: false });
+    }
+
+    // create user
+    await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      profileImageUrl,
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create user | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
 
 const logout = (req, res) => {};
 
